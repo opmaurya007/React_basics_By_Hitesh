f9ec4684c7771954c65d5bc4469143c8224bfba4
opmaurya007 <opmaurya970@gmail.com>
2023-10-05 14:52:09 +0530

    theme changer

--- 09themeFeature/src/App.jsx
+++ 09themeFeature/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/Theme'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode]=useState("light")
+  
+  const lightTheme=()=>{
+    setThemeMode('light')
+  }
+
+  const darkTheme=()=>{
+    setThemeMode('dark')
+  }
+
+  // actual change in theme
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+  
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className='flex flex-arap min-h-sceen items-center'>
+      <div className='w-full'>
+        <div className='w-full max-w-sm mx-auto flex item-center justify-end mb-4'>
+
+        </div>
+
+        <div className='w-full max-w-sm mx-auto'>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+     
+    </ThemeProvider>
   )
 }
 
